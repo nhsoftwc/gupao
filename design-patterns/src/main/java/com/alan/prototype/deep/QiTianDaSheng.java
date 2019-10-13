fd57f87cf6fbfac45423afa90e137d6a24bad44d
alan.w <nhsoft.wc@outlook.com>
2019-10-13 11:48:51 +0800

    原型模式的修改

--- design-patterns/src/main/java/com/alan/prototype/deep/QiTianDaSheng.java
+++ design-patterns/src/main/java/com/alan/prototype/deep/QiTianDaSheng.java
@@ -1,5 +1,7 @@
 package com.alan.prototype.deep;
 
+import org.apache.commons.beanutils.BeanUtils;
+
 import java.io.*;
 import java.util.Date;
 
@@ -8,10 +10,14 @@ public class QiTianDaSheng extends Monkey implements Cloneable, Serializable {
     public JinGuBang jinGuBang;
 
     public QiTianDaSheng() {
+        // 只是初始化
         this.birthday = new Date();
         this.jinGuBang = new JinGuBang();
     }
 
+    // 克隆方法采用的是深克隆
+    // 可能会出现单例被破坏的问题
+    // 原型模式和单例模式唱反调的意思
     @Override
     protected Object clone() throws CloneNotSupportedException {
         return this.deepClone();
@@ -20,6 +26,9 @@ public class QiTianDaSheng extends Monkey implements Cloneable, Serializable {
     public Object deepClone() {
 
         try {
+
+            // 内存中完成操作，对象读写是通过字节码直接操作
+            // 跟序列化操作类似
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ObjectOutputStream oos = new ObjectOutputStream(bos);
             oos.writeObject(this);
@@ -27,6 +36,7 @@ public class QiTianDaSheng extends Monkey implements Cloneable, Serializable {
             ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
             ObjectInputStream ois = new ObjectInputStream(bis);
 
+            // 完整的新的对象
             QiTianDaSheng copy = (QiTianDaSheng) ois.readObject();
             copy.birthday = new Date();
             return copy;
@@ -37,6 +47,7 @@ public class QiTianDaSheng extends Monkey implements Cloneable, Serializable {
 
     }
 
+    // 浅克隆
     public QiTianDaSheng shallowClone(QiTianDaSheng target) {
 
         QiTianDaSheng qiTianDaSheng = new QiTianDaSheng();
